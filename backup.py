29245bfda2ebe2c6f0f407c2674c3f92addb6239
Dennis Juhler Aagaard <dennis@kulmosen.dk>
2015-03-19 23:22:03 +0100

    Added sendmail to avoid SMTPlib in python. Added first time backup mail also.

--- backup.py
+++ backup.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 #
-#FreeNAS Configuration Backup Script (version 1.0_15)
+#FreeNAS Configuration Backup Script (version 3)
 #Original script Created By: Eric Bright Copyright (C) 2013
 #Added mail feature by: Dennis Juhler Aagaard
 #
@@ -19,7 +19,8 @@
 #the location of the backup is in zpool/.system/cores/
 #in this way the config file is both protected by the ZFS and offsite
 #backup, with a nice history.
-#log location has been changed to /var/log/FreeNASconf-backup.log
+#log location has been changed to /var/log/FreeNASConf-backup.log
+
 
 import os
 import shutil
@@ -31,50 +32,44 @@ import getopt
 import optparse
 import subprocess
 
-#Define the arguments and add help describtions
-parser = optparse.OptionParser()
-parser.add_option('-n', '--name', help="Name of the system", action="store")
-parser.add_option('-m', '--mailserver', help="Hostname of mailserver with port; hostname.org:25", action="store")
-parser.add_option('-u', '--user', help="Username for the mail account", action="store")
-parser.add_option('-p', '--passwd', help="Password for your mail account. Beaware that this is in cleartext. This password will also be used for encryption.", action="store")
-parser.add_option('-t', '--emailto', help="Mail you want to send the file to", action="store")
-parser.add_option('-f', '--emailfrom', help="Naming of the email adress sent from. Mostly the same as your mailaccount", action="store")
+from subprocess import Popen, PIPE
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.utils import formataddr
+from email.header import Header
 
-#Default arguments.
-#CHANGE THESE TO YOUR EMAILACCOUNT INFO
-#==========VERY IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!!!
-#==!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-parser.set_defaults(name='Default FreeNAS Name')
-parser.set_defaults(mailserver='smtp.gmail.com')
-parser.set_defaults(user='CHANGE-THIS@gmail.com')
-parser.set_defaults(passwd='PASSWORD-CHANGE-THIS')
-parser.set_defaults(emailto='CHANGE-THIS@gmail.com')
-parser.set_defaults(emailfrom='CHANGE-THIS@gmail.com')
+localhost = socket.gethostname()
+
+#Define the arguments and add help describtions. YOU NEED TO CHANGE THE DEFAULT VALUES!!!!!!!!!!!!!!
+
+parser = optparse.OptionParser()
+parser.add_option('-n', '--name', default='Default FreeNAS Name', help="Name of the system", action="store")
+parser.add_option('-p', '--passwd', default='defaultpassword', help="Beaware that this is in cleartext. This password will also be used for encryption.", action="store")
+parser.add_option('-t', '--emailto', default='default@domain.com', help="Mail you want to send the file to", action="store")
+parser.add_option('-f', '--emailfrom', default='default@domain.com', help="Naming of the email adress sent from. Mostly the same as your mailaccount", action="store")
+
 options, args = parser.parse_args()
 
-
-#==================================================================================
-
 name = systemname = options.name
-mailserver = systemname = options.mailserver
-user = systemname = options.user
 passwd = systemname = options.passwd
 emailto = systemname = options.emailto
 emailfrom = systemname = options.emailfrom
 
-#Define all other Variables
+#Define all other Variables:
+
 d = datetime.datetime.now()
 date_format = "%Y_%m_%d_%H_%M"
-log_file = "/var/log/FreeNASconf-backup.log"
+log_file = "/var/log/FreeNASConf-backup.log"
 db_file = "/data/freenas-v1.db"
 bk_path = os.path.join(subprocess.check_output("zfs list | grep cores | awk -vORS= '{ print $5 }'", shell=True), '')
-backup_name = "backup"
-archive_name = "archive"
+backup_name = "FreeNAS-backup"
+archive_name = "FreeNAS-archive"
 ext = ".db"
 enc = ".enc"
-localhost = socket.gethostname()
+
 systemname = options.name
-UUID = subprocess.check_output("dmidecode | grep UUID", shell=True)
+UUID = subprocess.check_output("dmidecode | grep UUID | sed 's/.*: //'", shell=True)
 
 matched = None
 fname = None
@@ -90,11 +85,12 @@ logging.basicConfig(filename=log_file,
 logging.getLogger('').addHandler(console)
 
 if os.stat(log_file)[6]==0:
-	logging.debug('Version 2.0 - Encrypted email with default args')
+	logging.debug('Version 3.0 ')
 	logging.debug('Automatic Backup Script for FreeNAS Configuration File')
 	logging.debug('Original Script Created By: Eric Bright Copyright (C) 2013')
 	logging.debug('https://github.com/ebright/FreeNas_Config/')
-	logging.debug('Initial script made by Dennis Juhler Aagaard')
+	logging.debug('Refined by Dennis Juhler Aagaard')
+	logging.debug('https://github.com/kulmosen/Freenas-bk-cfg')
 	logging.debug('---------------------------')
 
 os.chdir(bk_path)
@@ -125,43 +121,93 @@ if matched is True:
 		
 		subprocess.call(['openssl', 'aes-256-cbc', '-a', '-salt', '-in', bk_path + backup_name + '_' + d.strftime(date_format) + ext, '-out', bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc, '-k', passwd])
 		
-		
-		#Gather all the Mailserver details + building up the mail containing text and the db file for offsite backup
-		import smtplib
-		from email.mime.multipart import MIMEMultipart
-		from email.mime.text import MIMEText
-		from email.header import Header
-		from email.utils import formataddr
-		
+
+		#Building the email with attachment using sendmail in system going away from SMTP lib
+
+		#Define variables in mail
 		attachment = MIMEText(file(bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc).read())
 		filename = file(backup_name + '_' + d.strftime(date_format) + ext)
 		date = d.strftime(date_format)
-		filename2 = 'FreeNAS_Conf_%s_%s%s%s' % (backup_name, date, ext, enc)
+		filename2 = '%s_%s%s%s' % (backup_name, date, ext, enc)
+		#fromaddr = formataddr((str(Header(u'%s' % name , 'utf-8')), "%s" % emailfrom))
+		#sendername = '%s <%s>' % (name, emailfrom)
+		fromaddr = '%s <%s>' % (name, emailfrom)
 		msg = MIMEMultipart()
-		s = smtplib.SMTP('%s' % mailserver)
-		s.starttls()
-		username = '%s' % user
-		password = '%s' % passwd
-		s.login(username,password)
-		toEmail = '%s' % emailto
-		fromEmail = formataddr((str(Header(u'%s' % systemname , 'utf-8')), "%s" % emailfrom)) 
 		msg['Subject'] = 'Backup of FreeNAS Configfile in hostname: %s. with the name: %s.' % (localhost, systemname)
-		msg['From'] = fromEmail
-		attachment.add_header('Content-Disposition', 'attachment; filename="%s"' % filename2)
-		body = 'You have recieved this mail because the FreeNAS configurations on \n \n %s \n \n with the name \n \n %s \n \n %s \n has been changed... \n' % (localhost, systemname, UUID)
-		content = MIMEText(body, 'plain')
-		body2 = "           \n"
-		content2 = MIMEText(body2, 'plain')
-		msg.attach(content)
-		msg.attach(content2)
-		msg.attach(attachment)
-		s.sendmail(fromEmail, toEmail, msg.as_string())
+		#msg['From'] = '%s' % fromaddr
+		msg['From'] = formataddr((str(Header(u'%s' % name , 'utf-8')), "%s" % emailfrom))
+		msg['Reply-to'] = '%s@%s' % (name, localhost)
+		msg['To'] = '%s' % emailto
+		
+		 
+		# That is what u see if dont have an email reader:
+		msg.preamble = 'Multipart massage.\n'
+		 
+		# This is the textual part:
+		part = MIMEText('You have recieved this mail because the FreeNAS configurations on \n \n %s \n \n with the name \n \n %s \n \n UUID: %s \n has been changed... \n' % (localhost, systemname, UUID))
+		msg.attach(part)
+		 
+		# This is the binary part(The Attachment):
+		part = MIMEApplication(open("%s%s" % (bk_path, filename2),"rb").read())
+		part.add_header('Content-Disposition', 'attachment', filename="%s" % filename2)
+		msg.attach(part)
+		 
+		p = Popen(["/usr/sbin/sendmail", "-t", "-oi", "-f", '%s <%s>' % (name, emailfrom)], stdin=PIPE)
+		p.communicate(msg.as_string())
+		print "successfully sent email"
 		
 		#remove the encrypted file after sending it. 
 		subprocess.call(['rm', '-R', bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc]) 
-					
+
+
+
 else:
 	bk_file = bk_path + backup_name + '_' + d.strftime(date_format) + ext
 	logging.info('No previous backup found. Create new backup: ' + bk_file)
 	shutil.copy2(db_file, bk_file)
+	subprocess.call(['openssl', 'aes-256-cbc', '-a', '-salt', '-in', bk_path + backup_name + '_' + d.strftime(date_format) + ext, '-out', bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc, '-k', passwd])
+		
+
+	#Building the email with attachment using sendmail in system going away from SMTP lib
+
+	#Define variables in mail
+	attachment = MIMEText(file(bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc).read())
+	filename = file(backup_name + '_' + d.strftime(date_format) + ext)
+	date = d.strftime(date_format)
+	filename2 = '%s_%s%s%s' % (backup_name, date, ext, enc)
+	#fromaddr = formataddr((str(Header(u'%s' % name , 'utf-8')), "%s" % emailfrom))
+	#sendername = '%s <%s>' % (name, emailfrom)
+	fromaddr = '%s <%s>' % (name, emailfrom)
+	msg = MIMEMultipart()
+	msg['Subject'] = 'Backup of FreeNAS Configfile in hostname: %s. with the name: %s.' % (localhost, systemname)
+	#msg['From'] = '%s' % fromaddr
+	msg['From'] = formataddr((str(Header(u'%s' % name , 'utf-8')), "%s" % emailfrom))
+	msg['Reply-to'] = '%s@%s' % (name, localhost)
+	msg['To'] = '%s' % emailto
+	
+	 
+	# That is what u see if dont have an email reader:
+	msg.preamble = 'Multipart massage.\n'
+	 
+	# This is the textual part:
+	part = MIMEText('You have recieved this mail because the FreeNAS configurations on \n \n %s \n \n with the name \n \n %s \n \n UUID: %s \n has been changed... \n' % (localhost, systemname, UUID))
+	msg.attach(part)
+	 
+	# This is the binary part(The Attachment):
+	part = MIMEApplication(open("%s%s" % (bk_path, filename2),"rb").read())
+	part.add_header('Content-Disposition', 'attachment', filename="%s" % filename2)
+	msg.attach(part)
+	 
+	p = Popen(["/usr/sbin/sendmail", "-t", "-oi", "-f", '%s <%s>' % (name, emailfrom)], stdin=PIPE)
+	p.communicate(msg.as_string())
+	print "successfully sent email"
+	
+	#remove the encrypted file after sending it. 
+	subprocess.call(['rm', '-R', bk_path + backup_name + '_' + d.strftime(date_format) + ext + enc]) 
 sys.exit()
+
+
+
+
+
+
